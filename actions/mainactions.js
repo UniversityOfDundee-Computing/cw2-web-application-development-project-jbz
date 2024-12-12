0ea5f475de1b840bde32b9f39dbad638f1f8f77c
f-zaryab <zaryabfarooqui@gmail.com>
2024-12-12 18:38:01 +0000

    sentiment-api integrated

--- actions/mainactions.js
+++ actions/mainactions.js
@@ -1,4 +1,38 @@
-const heading = document.getElementById("head");
-const text = heading.innerText;
+const sentimentForm = document.getElementById("search-mood");
 
-console.log("Heading Text: ", text);
+async function query(data) {
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/SamLowe/roberta-base-go_emotions",
+    {
+      headers: {
+        Authorization: "Bearer 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",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  return result;
+}
+
+sentimentForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  // Get the value of the query input field
+  const queryInput = document.getElementById("moodQuery").value;
+
+  query({
+    inputs: queryInput,
+  }).then((response) => {
+    const results = response[0].filter((res) => res.score > 0.005);
+    console.log(results[0]);
+  });
+});
+
+// query({
+//   inputs: "i am feeling down",
+// }).then((response) => {
+//   const results = response[0].filter((res) => res.score > 0.005);
+//   console.log(results[0]);
+// });
